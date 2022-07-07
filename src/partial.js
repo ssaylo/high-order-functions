0307d60634467b404860af3deeeab4efe097bb17
ssaylo <ssaylo@163.com>
2022-07-08 00:18:07 +0800

    chore: commet

--- src/partial.js
+++ src/partial.js
@@ -1,18 +1,19 @@
 const _ = require('lodash')
 
-// 偏函数
+// 偏函数，讲道理这个偏函数没什么用
 
 function buildUri(scheme, domain, path) {
   return `${scheme}://${domain}/${path}`;
 }
 
-const myGithubPath = _.partial(buildUri, "https", "github.com");
+const myGithubPath = partial(buildUri, "https", "github.com");
 const profilePath = myGithubPath("linlai/linlai163");
 const awesomeTsPath = myGithubPath("linlai/awesome-linlai");
 
-console.log(myGithubPath, profilePath, awesomeTsPath);
+// console.log(myGithubPath, profilePath, awesomeTsPath);
 
 function partial(fn) {
+  console.log(arguments)
   let args = [].slice.call(arguments, 1);
   return function () {
     const newArgs = args.concat([].slice.call(arguments));
